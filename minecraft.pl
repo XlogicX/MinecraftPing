5d7fb9983f89a1f0feb98f2551abab1fc026adb8
XlogicX <no.axiom@gmail.com>
2013-05-02 22:46:14 -0700

    Added feature to feed in nmap -oG results

--- minecraft.pl
+++ minecraft.pl
@@ -1,4 +1,11 @@
 #!/usr/bin/perl
+#Version 1.1 of Minecraft ping scanner
+#This version adds the ability to take nmap outputs of -oG as an input to this script
+#Next version will clean the code up a little (organize with some subs maybe).
+#I will also add some documentation.
+#There will likely be large bug fixes, I'm sure I'll see unexpected things in -->
+#things in -oG files, or get odd things back from minecraft servers that I -->
+#didn't anticipate to parse
 use strict;
 use warnings;
 use IO::Socket;
@@ -8,18 +15,40 @@ use Getopt::Long;
 #$recv is the container of the unparsed data, and $server is just an IP address
 my ($protocol, $version, $message, $people, $capacity, $recv, $server);
 #These are vars for the input file, either a line by line list of ip's, or
-#an -oG output from Nmap (not yet implemented
+#an -oG output from Nmap
 my ($iplist,$nmap) = 0;
+my @servers;
 
-GetOptions('iplist=s' => \$iplist,  	#get list of IP addresses
+GetOptions('iplist=s' => \$iplist,		#get list of IP addresses
 		'nmap=s' => \$nmap);			#get lines from nmap
+
+if (($iplist ne 0) && ($nmap ne 0)) {
+	print "Woah, wait a second, did you want a normal IP list, or an nmap -oG output list, because we can't have both\n";
+	exit 0;
+}
 
 #this is the simple way to give this script IP's; just a text file with IP's
 if ($iplist ne 0) {
     open IPS, "$iplist";	
+    my @servers = <IPS>;	#make array of IP's from file handle
 }
 
-my @servers = <IPS>;	#make array of IP's from file handle
+if ($nmap ne 0) {
+	open IPS, "$nmap";		
+	my @nmaplines = <IPS>;		#make an array of all the -oG nmap lines
+	my $nhosts = @nmaplines;	#find out how many lines that is
+	my $inc = 0;				#set up an incrementer
+	while ($nhosts > 0){		#while there are still -oG nmap lines
+		#If the line indicates the port was open
+		if (($nmaplines[$nhosts]) && ($nmaplines[$nhosts] =~ /Host: ((\d+\.){3}\d+)\s+.+open/)) {
+			$servers[$inc] = "$1";	#grab the IP (regex for IP can be fairly non-explicit;
+									#as we don't really expect false positives in -oG outputs of nmape)
+			$inc++;					#increment our array for actual open minecraft servers
+		}
+		$nhosts--;					#decrement to lines of -oG output that we know of
+	}
+}
+
 my $hosts = @servers;	#get the total number of servers
 my $decrementer = $hosts;	#used for the while loop
 
@@ -52,3 +81,11 @@ while ($decrementer gt 0) {
 	close $socket;		#close this socket
 	$decrementer--;		#on to the next
 }
+
+#A typical nmap scan that provides a good input to this script.
+#I used -iL ips.txt with a known-open list of minecraft IP's.
+#I would probably do this even after scanning a large CIDR range just
+#to scrub the possible servers that went dead since. This would take
+#some load off of the minecraft.pl script (so it's not probing dead hosts)
+#In other words, let nmap do what it's good at.
+#sudo nmap -sV -PN -p 25565 -iL ips.txt -oG results.txt
